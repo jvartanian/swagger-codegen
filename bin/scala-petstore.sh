27a31ea0bf5d8d64b2e94e65be2dfe18b2e46eab
Justin Vartanian <justinvart@gmail.com>
2023-04-25 21:31:33 -0500

    Enable scala-petstore script

--- bin/scala-petstore.sh
+++ bin/scala-petstore.sh
@@ -26,6 +26,6 @@ fi
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -Dlogback.configurationFile=bin/logback.xml"
-ags="$@ generate -t modules/swagger-codegen/src/main/resources/scala -i modules/swagger-codegen/src/test/resources/2_0/petstore.yaml -l scala -o samples/client/petstore/scala"
+ags="$@ generate -i modules/swagger-codegen/src/test/resources/3_0_0/petstore-with-fake-endpoints-models-for-testing.yaml -l scala -o samples/client/petstore/scala"
 
-# java $JAVA_OPTS -jar $executable $ags
+java $JAVA_OPTS -jar $executable $ags
